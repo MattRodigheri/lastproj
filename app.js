ee1f724c9491d4a3b0334e82f02fdf07db146020
MattRodigheri <mrodigheri@gmail.com>
2018-07-20 10:45:22 -0700

    wrote plan, index.html and app.js o Please enter the commit message for your changes. Lines starting

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  //attach event listener to buttons(input?)
+  //create function stub
+
+
+$().on('click', function(){
+    
+  });
+
+});
---
+++ plan.js
@@ -0,0 +1,43 @@
+//Project Goals
+
+/*
+Accept Data - form, event listener, functio nto accept form data, input
+validation, different types of input, buttons
+
+Store Data - accept validated form data and write to local storage
+
+Retrieve Data - button, function to grab storage, function to update display
+
+Update Data -
+
+Delete Data - remove data from local storage
+
+MVP - A form with two fields, two buttons (one to get, one to set), able to get
+ and set data in local storage via the form. Basic UX and basic CSS
+
+√ 1. Create a form
+
+2. Add event listener(s)
+
+3. Learn about local storage
+
+4. implement basic write to local storage storage (hard code)
+
+5. implement basic read from local storage
+
+6. make read/write dynamic (attached to form events)
+6a. update data
+6b. delete data
+
+7. Basic CSS
+
+8. UX - Input confirmation/validation
+
+
+MVP+
+ - Ability to filter list (live?) via text
+ - Toaster Messages (popup confirmation message) - ipnut confirmation/success/fail message
+ - Manage local storage data based on purpose/intent
+ - Different users
+
+*/
